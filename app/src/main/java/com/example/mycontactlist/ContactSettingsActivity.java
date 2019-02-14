ae01dee178988f4dd78a583733a8967fb13ecd09
Charles Hoff <charles.hoff@gtri.gatech.edu>
2019-02-14 13:37:43 -0500

    Added some comments

--- app/src/main/java/com/example/mycontactlist/ContactSettingsActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactSettingsActivity.java
@@ -27,7 +27,7 @@ public class ContactSettingsActivity extends AppCompatActivity {
         prefs = getSharedPreferences("bgColor", MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
 
-        String colorStr = prefs.getString("color", "WHITE");
+        String colorStr = prefs.getString("color", "WHITE"); //String for storing the String value of the color in SharedPrefs
 
         if(colorStr.equals("BLUE")){
             scrollviewobject.setBackgroundResource(R.color.blue);
